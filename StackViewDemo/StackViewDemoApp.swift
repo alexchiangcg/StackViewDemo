108982dc00f3fd043c95981001a634f2a988b6e4
alexChiang <alexchiang.cg@gmail.com>
2023-09-19 17:11:03 +0800

    Initial Commit

---
+++ StackViewDemo/StackViewDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  StackViewDemoApp.swift
+//  StackViewDemo
+//
+//  Created by 江德宣 on 2023/9/19.
+//
+
+import SwiftUI
+
+@main
+struct StackViewDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
